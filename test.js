036a65f7c7fed2190aa31a1a771bf9325d6f9523
Manish Thakur <45007295+Manish-Thakur@users.noreply.github.com>
2019-12-19 08:02:12 -0800

    Add files via upload

---
+++ test.js
@@ -0,0 +1,23 @@
+// var str=prompt("are we there yet!");
+
+
+
+// while(str !== "yes" && str!=="yeah")
+// {
+// 	var str=prompt("are we there yet!");
+// }
+
+// alert("Yes, we made it!!");
+
+
+var str=prompt("are we there yet!");
+
+
+
+while(str.indexOf("yes")=== -1)
+{
+	var str=prompt("are we there yet!");
+}
+
+alert("Yes, we made it!!");
+
